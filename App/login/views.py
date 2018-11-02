64e73585674256d914f43f5d324c5cbc740a8b14
Mishawaka <budishmishka@gmail.com>
2018-11-02 21:21:42 +0200

    Before pulling

--- App/login/views.py
+++ App/login/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
-from . import camera
+# from . import camera
+import cv2
+vc = cv2.VideoCapture(0)
+
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return StreamingHttpResponse(gen(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+def gen():
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
 
 def index(request):
-    return render(request, 'login/index.html', {'camera': StreamingHttpResponse(camera.gen(camera.VideoCamera()))})
+    return render(request, 'login/index.html', {'camera': video_feed()})
