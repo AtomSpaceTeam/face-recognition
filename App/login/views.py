79cf8ae2fd1690cffb2bd402669371293af55470
Mishawaka <budishmishka@gmail.com>
2018-11-02 16:04:34 +0200

    Not working camera streaming

--- App/login/views.py
+++ App/login/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import StreamingHttpResponse
+from . import camera
 
 def index(request):
-    return render(request, 'login/index.html')
+    return render(request, 'login/index.html', {'camera': StreamingHttpResponse(camera.gen(camera.VideoCamera()))})
