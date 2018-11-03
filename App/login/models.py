418b5713f5d19ce44e39dfd7c4939c01ddb3d775
Mishawaka <budishmishka@gmail.com>
2018-11-03 14:38:47 +0200

    Ready db to web connection

--- App/login/models.py
+++ App/login/models.py
@@ -4,8 +4,10 @@ class Resident(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
     age = models.IntegerField()
-    last_entry = models.DateTimeField()
-    photo_1 = models.ImageField()
-    photo_2 = models.ImageField()
-    photo_3 = models.ImageField()
-    photo_4 = models.ImageField()
+    # last_entry = models.DateTimeField(auto_now_add=True)
+    # photo_2 = models.ImageField()
+    # photo_3 = models.ImageField()
+    # photo_4 = models.ImageField()
+    
+    def __str__(self):
+        return self.name
