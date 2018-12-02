a8c0e7cb76454d4339ab3f4deabf5cf65b5ed4de
max-osadchiy <max_cyber@ukr.net>
2018-12-02 10:53:40 +0200

    Add auth

--- App/login/urls.py
+++ App/login/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url, include
+from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^residents/', views.list, name='list')
+    path('', views.index, name='index'),
+    path('residents', views.list_residents, name='list_residents'),
+    path('mentors', views.list_mentors, name='list_mentors'),
+    path('owners', views.list_owners, name='list_owners'),
+    path('camera', views.online_camera, name='online_camera'),
+    path('login/', auth_views.LoginView.as_view(template_name='login/login.html')),
 ]
