7c8548a8ef4e2beedae1b35b84ffe42375182c49
Max Osadchyi <osadchyimax17@gmail.com>
2019-02-24 21:45:45 +0200

    update

--- src/components/UserPages/HomePage/Card/Card.jsx
+++ src/components/UserPages/HomePage/Card/Card.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import './Card.css';
 
+const { card_list } = this.props;
+
 const posts = [
   {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
   {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 ];
 
-const Card = (props) => (
-  <div key={props.id}>
-    <h3>{props.title}</h3>
-    <p>{props.content}</p>
-  </div>
+const Card = () => (
+  {card_list.map(props => (
+    <div key={props.id}>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  ))}
 );
 
 export default Card;
