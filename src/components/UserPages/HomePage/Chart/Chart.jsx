2b195c588680c56047a323170005b898756e595c
Mishawaka <budishmishka@gmail.com>
2019-02-26 21:05:55 +0200

    Part

--- src/components/UserPages/HomePage/Chart/Chart.jsx
+++ src/components/UserPages/HomePage/Chart/Chart.jsx
@@ -30,26 +30,27 @@ class Chart extends React.Component {
   }
 
   componentDidMount(){
-    fetch('http://localhost:8000/api/admin-attendance')
-    .then((res) => res.json())
+    fetch('http://localhost:8000/api-attendance')
+    .then((res) => res)
     .then((data) => {
+      console.log(data.toString())
       let labels = [];
       let datasets = [{label: 'Attendance in this month', data:[]}];
-      for (let i in data){
-        labels.push(i);
-        datasets[0].data.push(data[i])
-      }
-      let chartData = {
-        labels,
-        datasets
-      };
-      chartData.datasets[0].backgroundColor = 'rgba(255,99,132,0.2)';
-      chartData.datasets[0].borderColor = 'rgba(255,99,132,1)';
-      chartData.datasets[0].borderColor = 'rgba(255,99,132,1)';
-      chartData.datasets[0].hoverBackgroundColor = 'rgba(255,99,132,0.4)';
-      chartData.datasets[0].hoverBorderColor = 'rgba(255,99,132,1)';
-      this.setState({chartData});
-      console.log(this.state.chartData);
+      // for (let i in data){
+      //   labels.push(i);
+      //   datasets[0].data.push(data[i])
+      // }
+      // let chartData = {
+      //   labels,
+      //   datasets
+      // };
+      // chartData.datasets[0].backgroundColor = 'rgba(255,99,132,0.2)';
+      // chartData.datasets[0].borderColor = 'rgba(255,99,132,1)';
+      // chartData.datasets[0].borderColor = 'rgba(255,99,132,1)';
+      // chartData.datasets[0].hoverBackgroundColor = 'rgba(255,99,132,0.4)';
+      // chartData.datasets[0].hoverBorderColor = 'rgba(255,99,132,1)';
+      // this.setState({chartData});
+      // console.log(this.state.chartData);
     })
     .catch((err) => console.log(err))
   }
@@ -59,8 +60,6 @@ class Chart extends React.Component {
         <Bar 
           data={this.state.chartData}
           options={this.state.options}
-          width=''
-          height=''
         />
     );
   }
