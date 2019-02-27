1530158a5f86cacfa59f5398e480e6c575b4cb82
Max Osadchyi <osadchyimax17@gmail.com>
2019-02-27 17:31:33 +0200

    after pull

--- src/components/UserPages/PeoplePage/PeoplePage.jsx
+++ src/components/UserPages/PeoplePage/PeoplePage.jsx
@@ -2,33 +2,19 @@ import React from 'react';
 
 import './PeoplePage.css';
 
-const PeoplePage = () => (
-  <div className="people-block">
-    <div className="card-people">
-      <h3>Maxim Osadchiy</h3>
-      <p>Status: Resident</p>
-      <p>Team: Atom Team</p>
-      <p>Projects: Face Recognition</p>
-    </div>
-    <div className="card-people">
-      <h3>Maxim Osadchiy</h3>
-      <p>Status: Resident</p>
-      <p>Team: Atom Team</p>
-      <p>Projects: Face Recognition</p>
-    </div>
-    <div className="card-people">
-      <h3>Maxim Osadchiy</h3>
-      <p>Status: Resident</p>
-      <p>Team: Atom Team</p>
-      <p>Projects: Face Recognition</p>
-    </div>
-    <div className="card-people">
-      <h3>Maxim Osadchiy</h3>
-      <p>Status: Resident</p>
-      <p>Team: Atom Team</p>
-      <p>Projects: Face Recognition</p>
-    </div>
-  </div>
-);
+class PeoplePage extends React.Component {
+  render() {
+    return (
+      <div className="people-block">
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PeoplePage;
