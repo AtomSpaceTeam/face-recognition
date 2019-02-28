82291264947d607046df5e6858beab7c3519ca44
Max Osadchyi <osadchyimax17@gmail.com>
2019-02-28 17:48:59 +0200

    After pull

--- src/components/UserPages/PeoplePage/PeoplePage.jsx
+++ src/components/UserPages/PeoplePage/PeoplePage.jsx
@@ -12,6 +12,36 @@ class PeoplePage extends React.Component {
           <p>Team: Atom Team</p>
           <p>Projects: Face Recognition</p>
         </div>
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
+        <div className="card-people">
+          <h3>Maxim Osadchiy</h3>
+          <p>Status: Resident</p>
+          <p>Team: Atom Team</p>
+          <p>Projects: Face Recognition</p>
+        </div>
       </div>
     );
   }
