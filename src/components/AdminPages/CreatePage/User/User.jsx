a57bf52e88fb45d4ce35451bef052bf5bbe25374
Mishawaka <budishmishka@gmail.com>
2019-03-12 20:57:09 +0200

    Added adaptive form to create user

--- src/components/AdminPages/CreatePage/User/User.jsx
+++ src/components/AdminPages/CreatePage/User/User.jsx
@@ -4,8 +4,18 @@ class CreateUser extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			loading: false
+			loading: false,
+			user: {},
+			type: 'text'
 		}
+	}
+
+	onFocus = () => {
+		this.setState({ type: 'date' })
+	}
+
+	onBlur = () => {
+		this.setState({ type: 'text' })
 	}
 
 	render() {
@@ -16,13 +26,47 @@ class CreateUser extends React.Component{
 			button = <button onClick={this.onSubmit} className='login-button' type="submit">Log In</button>
 		}	
 		return (
-			<div className={'login-form ' + this.props.class}>
+			<div className={'login-form'}>
 				<h2>Create new user</h2>
-				<div className="input-container">
-					<input onKeyPress={this.key} onChange={this.UsernameChange} type="text" placeholder="Username"/>
-				</div>
-				<div className="input-container">
-					<input onKeyPress={this.key} onChange={this.PasswordChange} type="password" placeholder="Password"/>
+				<div className="login-form-row">
+					<div className="form-left">
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="Username"/>
+						</div>
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="First Name"/>
+						</div>
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="Last Name"/>
+						</div>			
+						<div className="input-container">
+							<input onKeyPress={this.key} onFocus={ this.onFocus } onBlur={ this.onBlur }  type={this.state.type} placeholder="Birth Date"/>
+						</div>
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="E-mail"/>
+						</div>
+					</div>
+					<div className="form-right">
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="Team"/>
+						</div>
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="Project"/>
+						</div>
+						<div className="input-container">
+							<input onKeyPress={this.key} type="text" placeholder="Photo"/>
+						</div>
+						<div className="input-container">
+							<select onKeyPress={this.key} type="text" placeholder="Status">
+								<option value="" disabled selected>Select your option</option>
+								<option value="resident">Resident</option>
+								<option value="mentor">Mentor</option>
+							</select>
+						</div>
+						<div className="input-container">
+							<select onKeyPress={this.key} type="text" placeholder="Specialisation"></select>
+						</div>
+					</div>
 				</div>
 				{button}
 			</div>
